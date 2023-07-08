18da88e7c99892c742eb782a1bef859795a8f654
Robert Kottelin <robert.kottelin@gmail.com>
2023-07-08 16:48:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-// import logo from './Whale.png';
-import './App.css';
-import React, { Component } from 'react';
-import {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { ethers, Contract } from 'ethers';
 import Table from 'react-bootstrap/Table';
-import { render } from 'react-dom';
-import { Series, DataFrame } from 'pandas-js';
-const { ethers, Contract } = require('ethers')
-//const player = require('play-sound')(opts = {})
+import './App.css';
 
 const rpcURL = 'https://cloudflare-eth.com/'
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
 
 const usdc_abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
-
-const USDCCONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' // USDC
-let USDCcontract = new Contract(USDCCONTRACT_ADDRESS, usdc_abi, provider);
+const USDC_CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const USDCContract = new Contract(USDC_CONTRACT_ADDRESS, USDC_ABI, provider);
 
 const tether_abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_upgradedAddress","type":"address"}],"name":"deprecate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},
 {"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"deprecated","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_evilUser","type":"address"}],"name":"addBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable",
@@ -26,101 +20,66 @@ const tether_abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name
 "name":"isBlackListed","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_clearedUser","type":"address"}],"name":"removeBlackList","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"MAX_UINT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_blackListedUser","type":"address"}],"name":"destroyBlackFunds","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialSupply","type":"uint256"},{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Issue","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Redeem","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newAddress","type":"address"}],"name":"Deprecate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"feeBasisPoints","type":"uint256"},{"indexed":false,"name":"maxFee","type":"uint256"}],"name":"Params","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_blackListedUser","type":"address"},{"indexed":false,"name":"_balance","type":"uint256"}],"name":"DestroyedBlackFunds","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"AddedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_user","type":"address"}],"name":"RemovedBlackList","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},
 {"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"}]
 
-const TetherCONTRACT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7' // Tether
-let Tethercontract = new Contract(TetherCONTRACT_ADDRESS, tether_abi, provider);
+const TETHER_CONTRACT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const TetherContract = new Contract(TETHER_CONTRACT_ADDRESS, TETHER_ABI, provider);
 
 
-// Note: USDC uses 6 decimal places
-const TRANSFER_THRESHOLD = 100000000 // wei
+const TRANSFER_THRESHOLD = ethers.utils.parseUnits('100', 'wei');
 
 const App = () => {
-  const tethername = Tethercontract.name()
-  const USDCname = USDCcontract.name()
-  //console.log(`Whale tracker started!\nListening for large transfers on ${USDCname} and ${tethername}`)
-  const [datalist, setData] = useState([])
+  const [dataList, setDataList] = useState([]);
 
-  var id = 1;
-  
-  USDCcontract.on('Transfer', (from, to, amount, data, transactionHash) => {
-      if(amount.toNumber() >= TRANSFER_THRESHOLD) {
-          id ++;
-          // console.log(`New whale transfer for ${USDCname}: https://etherscan.io/tx/${data.transactionHash}`)
-          // console.log('Amount:', amount.toNumber())
-          // console.log('From:', from)
-          // console.log('hash:', data.transactionHash)
-          const realAmount = amount.toNumber();
-          const hash = data.transactionHash;
-          setData(current => [...current, {id, hash, from, to, realAmount}]);
-          const uniqueList = getUnique(datalist,'hash');
-          console.log(uniqueList);
+  useEffect(() => {
+    const handleTransfer = (from, to, amount, event) => {
+      if (amount.gte(TRANSFER_THRESHOLD)) {
+        setDataList(oldDataList => [
+          ...oldDataList,
+          {
+            id: oldDataList.length + 1,
+            hash: event.transactionHash,
+            from,
+            to,
+            amount: ethers.utils.formatEther(amount),
+          },
+        ]);
       }
+    };
 
-  })
+    USDCContract.on('Transfer', handleTransfer);
+    TetherContract.on('Transfer', handleTransfer);
 
-  // Tethercontract.on('Transfer', (from, to, amount, data) => {
-  //     if(amount.toNumber() >= TRANSFER_THRESHOLD) {
-  //         // console.log(`New whale transfer for ${tethername}: https://etherscan.io/tx/${data.transactionHash}`)
-  //         // console.log('Amount:', amount.toNumber())
-  //         // console.log('From:', from)
-  //         // console.log('To:', to)    
-  //         setData(current => [...current, {id, from, to, amount}]);
-  //         id ++;
-  //         console.log(datalist);
-  //     }
-  // })
-
-  function getUnique(datalist, index) {
-
-    const unique = datalist
-         .map(e => e[index])
-  
-         // store the keys of the unique objects
-         .map((e, i, final) => final.indexOf(e) === i && i)
-    
-         // eliminate the dead keys & store unique objects
-        .filter(e => datalist[e]).map(e => datalist[e])
-
-        .sort(); 
-  
-     return unique;
-  }
+    return () => {
+      USDCContract.off('Transfer', handleTransfer);
+      TetherContract.off('Transfer', handleTransfer);
+    };
+  }, []);
 
   return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={} className="App-logo" alt="logo" /> */}
-          <p>
-            {/* {getUnique(datalist,'id').map(function(data, index){
-              return (<li key={index}>{data.id}</li>)
-            })} */}
-            {/* {JSON.stringify(getUnique(datalist,'id'))} */}
-          </p>
-            <table striped bordered hover size="sm">
-              <caption>Whale Tracker</caption>
-              <thead>
-                <tr>
-                  {/* <th>ID</th> */}
-                  <th>From</th>
-                  <th>To</th>
-                  <th>Amount</th>
-                  <th>TxnHash</th>
-                </tr>
-              </thead>
-              <tbody>
-                {getUnique(datalist,'hash').map(data => (
-                  <tr key={data.id}>
-                    {/* <td>{data.id}</td> */}
-                    <td>{data.from}</td>
-                    <td>{data.to}</td>
-                    <td>{data.realAmount}</td>
-                    <td>{data.hash}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-        </header>
-      </div>
-    );
-  };
+    <div className="App">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Hash</th>
+            <th>From</th>
+            <th>To</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataList.map(({ id, hash, from, to, amount }) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{hash}</td>
+              <td>{from}</td>
+              <td>{to}</td>
+              <td>{amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
 export default App;
